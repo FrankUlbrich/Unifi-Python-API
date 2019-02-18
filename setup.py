a0d58a2e0cf81ee3c412b7da1b60c6bb85216518
vagelim <vageli.code@gmail.com>
2019-02-18 14:16:40 -0500

    create python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ubiquiti',
+      version='0.2',
+      description='Library for interacting with Ubiquity Cloudkey',
+      url='http://github.com/vageli/Unifi-Python-API',
+      author='vagelim',
+      author_email='code@evangelosm.com',
+      license='MIT',
+      packages=['ubiquiti'],
+      zip_safe=False)
